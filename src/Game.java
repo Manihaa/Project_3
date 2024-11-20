d11da5f403a988462a9a867391339e8a53b48c64
manihaa <manihaa@nycstudents.net>
2024-11-20 11:01:33 -0500

    Added more methods to the Game class

---
+++ src/Game.java
@@ -0,0 +1,89 @@
+import java.util.Scanner;
+import java.util.*;
+public class Game {
+    // instance variables
+    private String name;
+    private String temp;
+    private String map;
+    private String letter;
+    private String number;
+    private String point;
+
+    private String ship1x;
+    private int ship1y;
+    private String ship2x;
+    private int ship2y;
+
+    // new objects
+    Scanner scan = new Scanner(System.in);
+    List<String> l1 = new ArrayList<>();
+
+    // constructor - add a parameter
+    public Game(){
+
+    }
+
+    public void start(){
+        System.out.print("Welcome to BattleShips!\nEnter your name: ");
+        name = scan.nextLine();
+        System.out.println("Hello, " + name + ", would you like to hear the rules? (y/n) ");
+        temp = scan.nextLine();
+        if (temp.equals("y")){
+            System.out.println();
+            // bunch of rules explained here
+        }
+
+    }
+
+    public void ask(){
+        System.out.println("Call your shot (Letter, #) ---> ");
+        point = scan.nextLine();
+        letter = point.substring(1, 2);
+        number = point.substring(4, 5);
+    }
+
+    public String map(){
+        list();
+        map = "      1    2    3    4    5\n";
+        for (int i = 0; i != 5; i++){
+            map += l1.get(i) + "|  ";
+
+                for (int j = 1; j != 6; j++){
+                    map += "  o  ";
+                }
+            map += "\n\n";
+        }
+        return map;
+    }
+
+    // helper method
+    private void list(){
+        l1.add("A");
+        l1.add("B");
+        l1.add("C");
+        l1.add("D");
+        l1.add("E");
+    }
+
+    private int xLocation(){
+        int num = (int)(Math.random() * 4 + 1);
+        return num;
+    }
+
+    private String yLocation(){
+        String lett = "";
+        int let = (int)(Math.random() * 4 + 1);
+        if (let == 1){
+
+        }else if (let == 2){
+
+        }else if (let == 3){
+
+        }else if (let == 4){
+
+        }else if (let == 5){
+
+        }
+        return lett;
+    }
+}
