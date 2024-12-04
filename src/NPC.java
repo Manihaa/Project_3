c14a7c134c0b497c8ea1d03f728c892cf7d53a3b
Maniha <manihaa@nycstudents.net>
2024-12-03 21:39:43 -0500

    Javadoc of the NPC class

--- src/NPC.java
+++ src/NPC.java
@@ -2,22 +2,49 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * This class represents the NPC player
+ * @author Maniha
+ */
 public class NPC {
     // new objects
     List<String> l1 = new ArrayList<>();
 
     // instance variables
+    /**
+     * Represents the scanner object for the class
+     */
     private Scanner scan;
+
+    /**
+     * Represents the map of the game
+     */
     private String map;
+    /**
+     * If the player hit a ship
+     */
     private boolean hit;
+    /**
+     * How many hits the player got
+     */
     private int hits;
 
     // (y, x) --> (Letter, #)
+    /**
+     * The location of the first ship
+     */
     private int ship1;
+    /**
+     * The location of the second ship
+     */
     private int ship2;
 
     // _____________________________
     // constructor
+    /**
+     *Creates NPC object
+     * @param scan Scanner object
+     */
     public NPC(Scanner scan){
         this.scan = scan;
         hit = false;
@@ -25,6 +52,12 @@ public class NPC {
         list();
     }
 
+    //constructor (unused)
+    /**
+     *Creates unused NPC object
+     * @param scan Scanner object
+     * @param num Number of hits the player got
+     */
     public NPC(Scanner scan, int num){
         this.scan = scan;
         hit = false;
@@ -34,11 +67,19 @@ public class NPC {
     // _____________________________
 
     // getter method
+    /**
+     *Gets the number of hits done by the player
+     * @return Number of hits by the player
+     */
     public int getHits(){
         return hits;
     }
 
     // method
+    /**
+     *Creates the map of the game
+     * @return The map of the game
+     */
     public String map(){
         map = "\n--------------------\n";
         int num = 0;
@@ -53,6 +94,10 @@ public class NPC {
     }
 
     // method for 2 ships
+    /**
+     *Assigns a location for two ships
+     * @param num Determined that two ships will be made
+     */
     public void shipMaker(int num){
         ship1 = Location();
         ship2 = Location();
@@ -64,6 +109,10 @@ public class NPC {
     }
 
     // method for 1 ship
+    /**
+     *Assigns a location for one ship
+     * @param num Determined that one ship will be made
+     */
     public void shipMaker(double num){
         ship1 = Location();
         System.out.println(ship1); //test
@@ -71,6 +120,12 @@ public class NPC {
     }
 
     // method for 2 ships
+    /**
+     *Determines whether the player's point was a hit or a miss
+     * @param num Determined that two ships will be made
+     * @param number The player's point
+     * @return Whether the point was a hit or miss
+     */
     public boolean hitOrMiss(int num, int number){
         if (number == ship1 || number == ship2){
             hit = true;
@@ -85,6 +140,12 @@ public class NPC {
     }
 
     // method for 1 ship
+    /**
+     *Determines whether the player's point was a hit or a miss
+     * @param num Determined that one ship will be made
+     * @param number The player's point
+     * @return Whether the point was a hit or miss
+     */
     public boolean hitOrMiss(double num, int number){
         if (number == ship1){
             hit = true;
@@ -99,6 +160,11 @@ public class NPC {
     }
 
     // method
+    /**
+     *Updates map to show the hit
+     * @param number The point
+     * @return Updated map
+     */
     public String hit(int number){
         String num = "" + number;
         int idx = map.indexOf(num);
@@ -110,6 +176,11 @@ public class NPC {
     }
 
     // method
+    /**
+     *Updates map to show the miss
+     * @param number The point
+     * @return Updated map
+     */
     public String miss(int number){
         String num = "" + number;
         int idx = map.indexOf(num);
@@ -123,6 +194,9 @@ public class NPC {
 
     // _____________________________
     // helper method
+    /**
+     *Private helper method that generates numbers 1-25 in a list
+     */
     private void list(){
         for (int i = 1; i != 26; i++){
             l1.add(String.valueOf(i));
@@ -130,9 +204,12 @@ public class NPC {
     }
 
     // helper method
+    /**
+     *Private helper method that creates a random point on a map
+     * @return Random point on a map
+     */
     private int Location(){
         int num = (int) ((int)(Math.random() * 3 + 1) * (Math.random() * 3 + 1));
         return num;
     }
-
 }
